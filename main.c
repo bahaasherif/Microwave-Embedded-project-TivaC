5f885b884ce85ff8a703c3a7725db09c6bc8e6ff
mahmoudisma3il18 <mody.bg12@gmail.com>
2022-04-20 23:31:58 +0200

    added GPIO driver and common macros file

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
-#include "TM4C123.h"
+#include "gpio.h"
 
 int main(void)
 {
+	GPIO_setupPinDirection(PORTF_ID,PIN1_ID,PIN_OUTPUT,PIN_NO_RESISTOR);
+	GPIO_writePin(PORTF_ID,PIN1_ID,LOGIC_HIGH);
 	
-	int x = 100;
 	while(1);
 }
